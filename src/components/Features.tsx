ae911deff936fe9fd3460f604fd2a45b71a8cb71
Ny Aina Tsilavina Razafimahatratra <tsilavina484@gmail.com>
2026-08-27 01:47:18 +0300

    update features, add button to add in myCart

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,14 +1,7 @@
 import { Badge } from "./ui/badge";
-import image from "../assets/growth.png";
-import image3 from "../assets/reflecting.png";
-import image4 from "../assets/looking-ahead.png";
 import TiltDemo from "./ui/tiltDemo";
-
-interface FeatureProps {
-  title: string;
-  description: string;
-  image: string;
-}
+import { Button } from "./ui/button";
+import { useCart } from "@/context/CartContext";
 
 type ProduitProps = {
   id: number;
@@ -26,90 +19,91 @@ const featureList: string[] = [
 ];
 
 export const Features = () => {
+  const {ajouterAuPanier} = useCart()
   const listeArticle: ProduitProps[] = [
     {
       id: 1,
       name: "SAINTO 1.5L",
       price: 2291.66,
       rate: 4.8,
-      path: "#"
+      path: "#",
     },
     {
       id: 2,
       name: "SAINTO 1L",
       price: 1388.33,
       rate: 4.7,
-      path: "#"
+      path: "#",
     },
     {
       id: 3,
       name: "SAINTO 0.5L",
       price: 1180,
       rate: 4.6,
-      path: "#"
+      path: "#",
     },
     {
       id: 4,
       name: "SAINTO 5L",
       price: 4166.66,
       rate: 4.9,
-      path: "#"
+      path: "#",
     },
     {
       id: 5,
       name: "Bonbonne 1ère Livraison",
       price: 73333.33,
       rate: 4.5,
-      path: "#"
+      path: "#",
     },
     {
       id: 6,
       name: "Bonbonne Recharge",
       price: 30000,
       rate: 4.8,
-      path: "#"
+      path: "#",
     },
     {
       id: 7,
       name: "ICE TEA pomme 1.5L",
       price: 6805,
       rate: 4.7,
-      path: "#"
+      path: "#",
     },
     {
       id: 8,
       name: "ICE TEA pêche 1.5L",
       price: 6805,
       rate: 4.9,
-      path: "#"
+      path: "#",
     },
     {
       id: 9,
       name: "ICE TEA citron 1.5L",
       price: 6805,
       rate: 4.6,
-      path: "#"
+      path: "#",
     },
     {
       id: 10,
       name: "ICE TEA pomme 0.5L",
       price: 2916.66,
       rate: 4.5,
-      path: "#"
+      path: "#",
     },
     {
       id: 11,
       name: "ICE TEA pêche 0.5L",
       price: 2916.66,
       rate: 4.8,
-      path: "#"
+      path: "#",
     },
     {
       id: 12,
       name: "ICE TEA citron 0.5L",
       price: 2916.66,
       rate: 4.7,
-      path: "#"
+      path: "#",
     },
   ];
 
@@ -131,17 +125,21 @@ export const Features = () => {
         ))}
       </div>
 
-        <div className="grid grid-cols-1 gap-6 place-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {listeArticle.map((item) => (
+      <div className="grid grid-cols-1 gap-6 place-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {listeArticle.map((item) => (
+          <div key={item.id} className="flex flex-col items-center gap-2">
             <TiltDemo
-              key={item.id}
               maxTilt={4}
               perspective={200}
               image={item.path}
               product={item}
             />
-          ))}
-        </div>
+            <Button size="sm" onClick={() => ajouterAuPanier(item)}>
+              Ajouter au panier
+            </Button>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
